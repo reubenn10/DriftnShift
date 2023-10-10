a107d8fb9bb4e795de90b5b1ef85c3f4c44cc1d9
reubenn10 <141303537+reubenn10@users.noreply.github.com>
2023-10-10 20:09:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    menu.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    menu.classList.remove('active');
+}
+
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{delay: 100, origin: 'top'})
+sr.reveal('.home',{delay: 100, origin: 'top'})
+
+sr.reveal('.form-container form',{delay: 500, origin: 'left'})
+sr.reveal('.heading',{delay: 500, origin: 'top'})
+
+sr.reveal('.ride-container .box',{delay: 300, origin: 'top'})
+
+sr.reveal('.services-container .box',{delay: 300, origin: 'top'})
+
+sr.reveal('.about-container .box',{delay: 300, origin: 'top'})
+sr.reveal('.about-img',{delay: 300, origin: 'top'})
+sr.reveal('.about-text',{delay: 300, origin: 'top'})
+
+sr.reveal('.reviews-contianer .box',{delay: 300, origin: 'top'})
+
+sr.reveal('.newsletter .box',{delay: 300, origin: 'bottom'})
